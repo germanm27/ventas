c7f16ad01abb92c1e4aea9abb38bc81a02426a39
gfmen <gfmen@DESKTOP-G5SB6IU>
2019-07-20 17:23:59 -0600

    Creando el programa hola mundo

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Munda");
+            MessageBox.Show("Hola Mundo");
         }
     }
 }
